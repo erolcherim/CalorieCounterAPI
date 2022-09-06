efac412d19784ea4073cf36daf61c481cf2e1a1f
erolcherim <erol.cherim@gmail.com>
2022-09-06 22:19:24 +0300

    Added GenericRepository class and interface with common CRUD operations

---
+++ CalorieCounterAPI/Repositories/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace CalorieCounterAPI.Repositories.GenericRepository
+{
+    public interface IGenericRepository<TEntity>
+    {
+        //Create
+        Task Create(TEntity entity);
+
+        //Read
+        Task<List<TEntity>> GetAll();
+        Task<TEntity> GetById(int id);
+        //Task<TEntity> GetByNameAsync(string name);
+
+        //Update
+        Task Update(TEntity entity);
+
+        //Delete
+        Task Delete(TEntity entity);
+
+        //Save
+        Task<bool> SaveAsync();
+    }
+}
