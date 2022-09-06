c809ebb1b6efeb4eed794a164112062eeb728427
erolcherim <erol.cherim@gmail.com>
2022-09-06 22:34:21 +0300

    Added UserLoginDTO

---
+++ CalorieCounterAPI/DAL/DTOs/UserLogin.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CalorieCounterAPI.DAL.DTOs
+{
+    public class UserLogin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
