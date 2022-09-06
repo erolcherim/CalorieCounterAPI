00054fef7b7b0ab3738769ba5039150e28724512
erolcherim <erol.cherim@gmail.com>
2022-09-06 22:40:29 +0300

    Added SessionTokenRepository

---
+++ CalorieCounterAPI/Repositories/SessionTokenRepository/ISessionTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using CalorieCounterAPI.DAL.Models;
+using CalorieCounterAPI.Repositories.GenericRepository;
+
+namespace CalorieCounterAPI.Repositories.SessionTokenRepository
+{
+{
+    public interface ISessionTokenRepository : IGenericRepository<SessionToken>
+    {
+        Task<SessionToken> GetByJTI(string jti);
+    }
+}
+}
---
+++ CalorieCounterAPI/Repositories/SessionTokenRepository/SessionTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using CalorieCounterAPI.DAL.Models;
+using CalorieCounterAPI.DAL;
+using CalorieCounterAPI.Repositories.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalorieCounterAPI.Repositories.SessionTokenRepository
+{
+    public class SessionTokenRepository : GenericRepository<SessionToken>, ISessionTokenRepository
+    {
+        public SessionTokenRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<SessionToken> GetByJTI(string jti)
+        {
+            return await _context.SessionTokens
+                .FirstOrDefaultAsync(t => t.Jti.Equals(jti));
+        }
+    }
+}
