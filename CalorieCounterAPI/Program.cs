50c5f0d8b088228f9cef88864792da976f8653d3
erolcherim <erol.cherim@gmail.com>
2022-09-06 23:03:07 +0300

    Added Session Token Validaton, Fixed Program.cs config

--- CalorieCounterAPI/Program.cs
+++ CalorieCounterAPI/Program.cs
@@ -1,9 +1,17 @@
 using CalorieCounterAPI.DAL;
 using CalorieCounterAPI.DAL.Models;
+using CalorieCounterAPI.DAL.Models.Constants;
 using CalorieCounterAPI.DAL.Seeders;
+using CalorieCounterAPI.DAL.Utils;
+using CalorieCounterAPI.Repositories.AuthRepositoryWrapper;
+using CalorieCounterAPI.Repositories.AuthWrapperRepository;
 using CalorieCounterAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +20,74 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                },
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 //Config for EFCore Identity
 builder.Services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(UserRoleType.Admin, policy => policy.RequireRole(UserRoleType.Admin));
+    options.AddPolicy(UserRoleType.User, policy => policy.RequireRole(UserRoleType.User));
+});
+
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        RequireExpirationTime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is my custom secret key for auth")),
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero
+    };
+    options.Events = new JwtBearerEvents()
+    {
+        OnTokenValidated = SessionTokenValidator.ValidateSessionToken
+    };
+});
+
+//Services for AuthWrapper and UserService
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthWrapperRepository, AuthWrapperRepository>();
 builder.Services.AddScoped<InitialSeed>();
 
 string constr = builder.Configuration.GetConnectionString("Default");
