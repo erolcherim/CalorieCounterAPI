5a37c696f9dde018e84b49e4dd2e5fff39e32596
erolcherim <erol.cherim@gmail.com>
2022-09-06 22:52:47 +0300

    Added user login/generate token method to user service

--- CalorieCounterAPI/DAL/DTOs/UserLoginDTO.cs
+++ CalorieCounterAPI/DAL/DTOs/UserLoginDTO.cs
@@ -2,7 +2,7 @@
 
 namespace CalorieCounterAPI.DAL.DTOs
 {
-    public class UserLogin
+    public class UserLoginDTO
     {
         public string Email { get; set; }
         public string Password { get; set; }
