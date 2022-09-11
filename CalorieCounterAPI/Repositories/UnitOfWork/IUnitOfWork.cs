ee3483e3bbf059b179d2a5f54e3e0cd10c5a6579
mxrtinax <martinas_paul01@yahoo.com>
2022-09-11 22:49:50 +0300

    added users controller

--- CalorieCounterAPI/Repositories/UnitOfWork/IUnitOfWork.cs
+++ CalorieCounterAPI/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -1,11 +1,12 @@
 ﻿using CalorieCounterAPI.Repositories.FoodRepository;
+using CalorieCounterAPI.Repositories.UserRepository;
 
 namespace CalorieCounterAPI.Repositories.UnitOfWork
 {
     public interface IUnitOfWork
     {
         IFoodRepository Foods { get; }
-
+        IUserRepository Users { get; }
         int Save();
     }
 }
